88ed1bee484ae42eab33d672de659a460ac90276
urmillive <urmillive@gmail.com>
2024-06-30 19:38:04 +0530

    added server file and bounty files

---
+++ bounty1.js
@@ -0,0 +1,6 @@
+let sum = 0;
+for (let i = 0; i < 100000000000; i++) {
+    sum = sum + i;
+}
+
+console.log(sum);
---
+++ main.js
@@ -0,0 +1,93 @@
+// const arr = [ "ram", "lakhan", "kirat" ]
+
+// // const ages = [ 21, 22, 35, 54, 34 ];
+// // for (let age in ages) {
+// //     console.log(ages[ age ] % 2 !== 1 ? ages[ age ] : "");
+// // }
+
+// // let compare = ages[ 0 ];
+// // for (let i = 0; i < ages.length; i++) {
+// //     if (compare < ages[ i ]) {
+// //         compare = ages[ i ];
+// //     }
+// // }
+// // console.log(compare);
+
+
+
+// // const nums = [ 21, 22, 35, 54, 34 ];
+// // let reverseArr = [];
+// // for (let i = nums.length - 1; i >= 0; i--) {
+// //     reverseArr.push(nums[ i ])
+// // }
+// // console.log(reverseArr);
+
+// // const personArray = [ "harkirat", "rama", "priya" ];
+// // const genderArray = [ "male", "male", "female" ];
+
+// // for (let i = 0; i < personArray.length; i++) {
+// //     if (genderArray[ i ] == "male") {
+// //         console.log(personArray[ i ].charAt(0));
+// //     }
+// // }
+
+// const users = [ {
+//     firstName: "ram",
+//     gender: "male"
+// }
+//     , {
+//     firstName: "lakhan",
+//     gender: "male"
+// },
+// {
+//     firstName: "sita",
+//     gender: "female"
+// } ];
+
+// for (let i = 0; i < users.length; i++) {
+//     if (users[ i ].gender == "male") {
+//         console.log(users[ i ].firstName);
+//     }
+// }
+
+
+
+// function sum(num1, num2) {
+//     let result = num1 + num2;
+//     return display1(result);
+// }
+
+// function display1(data) {
+//     console.log('====================================');
+//     console.log("result of the sum is : ", data);
+//     return display2(data);
+//     console.log('====================================');
+// }
+
+// function display2(data) {
+//     console.log('====================================');
+//     console.log("Sum result is : ", data);
+//     console.log('====================================');
+// }
+
+// sum(1, 2)
+
+
+function sum(num1, num2, fnToCall) {
+    let result = num1 + num2;
+    fnToCall(result);
+}
+
+function display1(data) {
+    console.log('====================================');
+    console.log("result of the sum is : ", data);
+    console.log('====================================');
+}
+
+function display2(data) {
+    console.log('====================================');
+    console.log("Sum result is : ", data);
+    console.log('====================================');
+}
+
+sum(1, 2, display1);
